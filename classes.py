22483f11fa01ae264914c17d46f97301f448a759
matts1 <mattstark75@gmail.com>
2019-01-08 00:05:49 +1100

    Finalized class

--- classes.py
+++ classes.py
@@ -1,23 +1,48 @@
-from typing import TypeVar
+from typing import TypeVar, Optional, Generic
 
 
-class Base(object):
+class Character(object):
+    name: Optional[str]
+
+    def __init__(self):
+        self.name = None
+
+    def set_name(self, name):
+        self.name = name
+
+
+class Player(Character):
     pass
 
-class Subclass(Base):
-    pass
 
-def f1(a: Base) -> Base:
+def f1(a: Character) -> Character:
     return a
 
-v1 = f1(Subclass())
 
-T = TypeVar('T', bound=Base)
-def f2(a: T) -> T:
+v1 = f1(Player())
+
+C = TypeVar('C', bound=Character)
+
+
+def f2(a: C) -> C:
     return a
 
-v2 = f2(Subclass())
 
-@overload
-class vars
-method vars
+v2 = f2(Player())
+
+T = TypeVar('T')
+
+
+class Tree(Generic[T]):
+    def __init__(self, value: T, left: Optional['Tree[T]'] = None,
+            right: Optional['Tree[T]'] = None):
+        self.left = left
+        self.right = right
+        self.value = value
+
+
+Tree(1, Tree(0), Tree(1))
+# It can work out that this is invalid for you!
+Tree(1, Tree(0), Tree(1.0))
+
+# If we get to this stage, show off python stubs.
--- functions.py
+++ functions.py
@@ -4,18 +4,30 @@
 
 # ME: list -> union -> explain union returning union issues
 # ME: explain that you can still run the code if it doesn't type check correctly
+from numbers import Number
+from typing import TypeVar, List, Optional, Union, Iterator, Iterable, Callable, \
+    overload
+
+T = TypeVar('T')
+
+
 def get_only(l):
     return l[0]
 
 
-get_only([1, 2, 3])
-get_only(['a', 'b', 'c'])
+a = get_only([1, 2, 3])
+b = get_only(['a', 'b', 'c'])
 # ME: explain why this is a bad idea, shouldn't be allowed, alternatives.
-get_only([1, 'a'])
+c = get_only([1, 'a'])
 # ME: what happens here
-get_only([])
+# d = get_only([])
+# This is why the above is important
+# int_list = [1]
+# int_list.pop()
+# e = get_only(int_list)
+
 # ME: should we allow things like this.
-get_only({0: 'hello'})
+f = get_only({0: 'hello'})
 
 
 # ME: list -> iterable
@@ -33,17 +45,34 @@ my_sum([1.5, 2.5, 3.5], 0)
 my_sum(map(int, ['1', '2', '3']), 0)
 # ME: This will work, but explain numeric before this.
 my_sum(['a', 'b', 'c'], '')
+
+
 # This should break, but what if we could know about it at compile time.
-my_sum([1, 2, 3], 'a')
+# my_sum([1, 2, 3], 'a')
 
 
 # ME: show off callable
-def my_reduce(items, start, joiner):
+def my_reduce(items: Iterable[T], start: T, joiner: Callable[[T, T], T]):
     for item in items:
         start = joiner(start, item)
     return start
 
 
 n = my_reduce([1.0, 2.0, 3.0], 0.0, lambda x, y: x + y)
-if __name__ == '__main__':
-    print(get_only([]))
+
+
+def my_sum2(*args):
+    return sum(args)
+
+
+my_sum2(1, 2.0, 3)
+my_sum2(1.0, 2.0, 3.0)
+my_sum2(1.0, 2, 3.0)
+
+
+def most_common_name(**kwargs):
+    return max(kwargs, key=kwargs.get)
+
+
+# Don't try this at home, kids. This is not what you use keyword arguments for.
+most_common_name(alice=5, bob=4, eve=3)
